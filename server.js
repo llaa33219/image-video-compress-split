6e3c52a8f11ab4619d4d66a5b433c3b9d54978d3
BLOUplanet <147328072+llaa33219@users.noreply.github.com>
2025-10-08 19:08:47 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use(helmet({
     directives: {
       defaultSrc: ["'self'"],
       styleSrc: ["'self'", "'unsafe-inline'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
+      scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-hashes'"],
+      scriptSrcAttr: ["'unsafe-inline'"],
       imgSrc: ["'self'", "data:", "https:"],
       connectSrc: ["'self'"],
       fontSrc: ["'self'"],
